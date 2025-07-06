44af9c321e540ec48a3a251b07c6655db3b54f2b
mulsim <max2dgame12@gmail.com>
2025-07-06 21:40:00 +0300

    push project

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+import ReactDOM from "react-dom/client"
+import { Route, Routes, BrowserRouter } from "react-router-dom"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
